4f07b7f0bde9b379e2385ae3b1b13b594a3ec998
Jack Yuan <313981224+jackchangyuyuan@users.noreply.github.com>
2026-08-25 09:52:22 -0400

    chore(eslint-config): add TanStack Query lint rules

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,4 +1,5 @@
 import eslintReact from "@eslint-react/eslint-plugin";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 import { defineConfig } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -11,6 +12,7 @@ export const reactConfig = defineConfig([
       reactHooks.configs.flat.recommended,
       eslintReact.configs["disable-conflict-eslint-plugin-react-hooks"],
       reactRefresh.configs.vite,
+      tanstackQuery.configs["flat/recommended"],
     ],
   },
 ]);
